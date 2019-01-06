a153234b8af65246ce3d2447252f5099a3a9ad42
Hannan Hafeez <hannan.saddiqui@gmail.com>
2019-01-06 05:07:42 +0500

    Bouncing circle at the egdes
    Signed-off-by: Hannan Hafeez <hannan.saddiqui@gmail.com>

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -4,11 +4,37 @@ var btn = document.getElementById('draw');
 
 btn.onclick = drawShapes;
 
-canvas.width = window.innerWidth;
+canvas.width = window.innerWidth - 15;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+var x = 200;
+var y = 200;
+var dx = 5;
+var dy = 2;
+var radius = 30;
+
+function animate() {
+	requestAnimationFrame(animate);
+	// console.log('Lol');
+	c.clearRect(0, 0, innerWidth, innerHeight);
+	c.beginPath();
+	c.arc(x, y, radius, 0, Math.PI * 2);
+	c.strokeStyle = 'blue';
+	c.stroke();
+	// if(x>innerWidth) x = 0;		//To loop from left to right
+
+	if (x > canvas.width - radius || x < radius) {
+		dx *= (-1);
+	} //To bounce off the edges
+	if (y > canvas.height - radius || y < radius) {
+		dy *= (-1);
+	} //To bounce off the edges
+	x += dx;
+	y += dy;
+}
+animate();
 
 function drawShapes() {
 	c.clearRect(0, 0, canvas.width, canvas.height);
@@ -18,20 +44,10 @@ function drawShapes() {
 		const w = 10;
 		const h = w;
 
-		if (x > canvas.width - 10) {
-			x -= 10;
-		}
-		
-		if (y >canvas.height - 10) {
-			y -= 10;
-		}
-		if (x < w) {
-			x += 10;
-		}
-
-		if (y < w) {
-			y += 10;
-		}
+		if (x > canvas.width - 10) x -= 10;
+		if (y > canvas.height - 10) y -= 10;
+		if (x < w) x += 10;
+		if (y < w) y += 10;
 
 		c.beginPath();
 		c.fillStyle = `rgb(${255*Math.random()},${255*Math.random()},${255*Math.random()})`;
@@ -41,7 +57,5 @@ function drawShapes() {
 		// c.fillRect(x, y, w, h);
 	}
 }
-
-// c.fillRect(100, 100, 100, 100);
 
 console.log(canvas);
