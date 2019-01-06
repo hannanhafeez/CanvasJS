092588c06899ca61f545a613999c34c7839a3dae
Hannan Hafeez <hannan.saddiqui@gmail.com>
2019-01-07 01:09:48 +0500

    Multiple moving circles spawning within canvas
    Signed-off-by: Hannan Hafeez <hannan.saddiqui@gmail.com>

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -9,29 +9,56 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-var x = 200;
-var y = 200;
-var dx = 5;
-var dy = 2;
-var radius = 30;
+function Circle(x, y, dx, dy, rad, sColor) {
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.rad = rad;
+	this.strcolor = sColor;
+
+	this.draw = function () {
+		c.beginPath();
+		c.arc(this.x, this.y, this.rad, 0, Math.PI * 2);
+		c.strokeStyle = this.strcolor;
+		c.stroke();
+	}
+
+	this.update = function () {
+		if (this.x > canvas.width - this.rad || this.x < this.rad) {
+			this.dx *= (-1);
+		} //To bounce off the edges
+		if (this.y > canvas.height - this.rad || this.y < this.rad) {
+			this.dy *= (-1);
+		} //To bounce off the edges
+		this.x += this.dx;
+		this.y += this.dy;
+		this.draw();
+	}
+}
+
+
+var circleArr = [];
+
+
+for (let i = 0; i < 100; i++) {
+	var radius = 30;
+	var x = Math.random() * (canvas.width - radius * 2) + radius;
+	var y = Math.random() * (canvas.height - radius * 2) + radius;
+	var dx = 2 * (Math.random() - 0.5);
+	var dy = 2 * (Math.random() - 0.5);
+	var sCol = `rgb(${255 * Math.random()},${255 * Math.random()},${255 * Math.random()})`;
+	circleArr.push(new Circle(x, y, dx, dy, radius, sCol));
+}
+
 
 function animate() {
 	requestAnimationFrame(animate);
-	// console.log('Lol');
 	c.clearRect(0, 0, innerWidth, innerHeight);
-	c.beginPath();
-	c.arc(x, y, radius, 0, Math.PI * 2);
-	c.strokeStyle = 'blue';
-	c.stroke();
-	// if(x>innerWidth) x = 0;		//To loop from left to right
-	if (x > canvas.width - radius || x < radius) {
-		dx *= (-1);
-	} //To bounce off the edges
-	if (y > canvas.height - radius || y < radius) {
-		dy *= (-1);
-	} //To bounce off the edges
-	x += dx;
-	y += dy;
+
+	circleArr.forEach(element => {
+		element.update();
+	});
 }
 animate();
 
